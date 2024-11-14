5a92fe8c5656dc8124fce9a3eafa600f9ff4af4b
pratik2405 <shinde7388pratik@gmail.com>
2024-11-14 09:28:30 +0530

    Add project files.

---
+++ CRUD_Operation/Product.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Operation
+{
+    public class Product
+    {
+        //private int id;
+        //private string name;
+        //private int price;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+
+           
+
+        public override string ToString()
+        {
+            return $"{Id} , {Name} ,{Price}";
+        }
+    }
+
+    public class ProductCrud
+    {
+        List<Product> Products;
+
+        //Constructor
+        public ProductCrud()
+        {
+            Products = new List<Product>();
+        }
+
+        //Method to add product
+        public void AddProduct(Product p)
+        {
+            Products.Add(p);
+        }
+
+        //Method to display all products
+        public List<Product> DisplayAllProduct()
+        {
+           return Products; 
+        }
+
+        //Method to modify product
+
+        public void ModifyProduct(Product p) 
+        {
+            for(int i = 0; i < Products.Count; i++)
+            {
+                if (Products[i].Id == p.Id)
+                {
+                    Products[i].Name = p.Name;
+                    Products[i].Price = p.Price;
+                    break;
+                }
+            }
+        }
+
+        //Method to delete product
+
+        public void DeleteProduct(int id)
+        {
+            for (int i = 0; i < Products.Count; i++)
+            {
+                if (Products[i].Id == id)
+                {
+                    Products.Remove(Products[i]);
+                }
+            }
+        }
+
+        //Method to get product by ID
+
+        public Product GetProductById(int id)
+        {
+            Product p = new Product();
+
+            foreach (var item in Products) 
+            {
+                if (item.Id == id)
+                {
+                    item.Id = id;
+                    item.Name=p.Name;
+                    item.Price=p.Price;
+                    break;
+                }
+            }
+            return p;
+        }
+    }
+}
---
+++ CRUD_Operation/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Operation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCrud crud = new ProductCrud();
+
+            try
+            {
+                string ch;
+                do
+                {
+                    Console.WriteLine("1.Product List");
+                    Console.WriteLine("2.Add Product");
+                    Console.WriteLine("3.Modify Product");
+                    Console.WriteLine("4.Delete Product");
+                    Console.WriteLine("5.Get Product By Id");
+                    Console.WriteLine("Select option from above");
+
+                    int op = Convert.ToInt32(Console.ReadLine());
+
+                    switch (op)
+                    {
+                        case 1:
+                            List<Product> products = crud.DisplayAllProduct();
+                            foreach (Product product in products)
+                            {
+                                Console.WriteLine(product);
+                            }
+                            break;
+                        case 2:
+                            Product p = new Product();
+                            Console.WriteLine("Enter Id : ");
+                            p.Id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter Name :");
+                            p.Name = Console.ReadLine();
+                            Console.WriteLine("Enter Price :");
+                            p.Price = Convert.ToInt32(Console.ReadLine());
+
+                            crud.AddProduct(p);
+                            Console.WriteLine("Product Added Successfully");
+                            break;
+                        case 3:
+                            Product p1 = new Product();
+                            Console.WriteLine("Enter existing Id : ");
+                            p1.Id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter new Name :");
+                            p1.Name = Console.ReadLine();
+                            Console.WriteLine("Enter new Price :");
+                            p1.Price = Convert.ToInt32(Console.ReadLine());
+                            crud.ModifyProduct(p1);
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter existing ID that you want to delete :");
+                            int id = Convert.ToInt32(Console.ReadLine());
+                            crud.DeleteProduct(id);
+                            Console.WriteLine("Product Delete Successfully");
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter product id :");
+                            int id1 = Convert.ToInt32(Console.ReadLine());
+                            crud.GetProductById(id1);
+                            break;
+                    }
+                    Console.WriteLine("Do you want to continue ! (y/n)");
+                    ch = Console.ReadLine();
+                } while (ch == "yes" | ch == "y");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
